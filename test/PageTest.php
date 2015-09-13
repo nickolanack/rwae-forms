e66aeb9feabd4bf2f95f033840984252827b2d29
nickolanack <nickblackwell82@gmail.com>
2015-09-13 02:27:24 -0700

    add php test

---
+++ test/PageTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author nblackwe
+ *
+ */
+class FormTest extends PHPUnit_Framework_TestCase {
+
+    protected function _includeScaffolds() {
+}
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testWriteForms() {
+
+        $this->assertTrue(true);
+    }
+}
